4810faa530efcbaee908ee9db7727816eb94ca74
mh <mh@immerda.ch>
2009-06-01 21:42:32 +0200

    added statefile to be created with correct permissions

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,6 +1,12 @@
 class mailman::munin {
+  file{'/var/lib/munin/plugin-state/munin-mailman-log.state':
+    ensure => file,
+    replace => false,
+    owner => nobody, group => mailman, mode => 0640;
+  }
   munin::plugin{'mailman':
     config => "timeout 40
-group mailman";
+group mailman",
+    require => File['/var/lib/munin/plugin-state/munin-mailman-log.state'];
   }
 }
