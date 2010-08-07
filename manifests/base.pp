f824976343d88ae60d50dff4b7d3a17e25b43e4a
mh <mh@immerda.ch>
2010-08-07 02:12:18 +0200

    remove unnecessary fileserver variable (#2460)

--- manifests/base.pp
+++ manifests/base.pp
@@ -12,10 +12,10 @@ class mailman::base {
   }
 
   file{'/usr/local/mailman/Mailman/mm_cfg.py':
-    source => [ "puppet://$server/modules/site-mailman/config/${fqdn}/mm_cfg.py",
-                "puppet://$server/modules/site-mailman/config/mm_cfg.py",
-                "puppet://$server/modules/mailman/config/${operatingsystem}/mm_cfg.py",
-                "puppet://$server/modules/mailman/config/mm_cfg.py" ],
+    source => [ "puppet:///modules/site-mailman/config/${fqdn}/mm_cfg.py",
+                "puppet:///modules/site-mailman/config/mm_cfg.py",
+                "puppet:///modules/mailman/config/${operatingsystem}/mm_cfg.py",
+                "puppet:///modules/mailman/config/mm_cfg.py" ],
     require => Package['mailman'],
     notify => Service['mailman'],
     owner => root, group => mailman, mode => 0644;
--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,8 +1,8 @@
 class mailman::centos inherits mailman::base {
     file{'/etc/httpd/conf.d/mailman.conf':
-        source => [ "puppet://$server/modules/site-mailman/httpd/${fqdn}/mailman.conf",
-                    "puppet://$server/modules/site-mailman/httpd/mailman.conf",
-                    "puppet://$server/modules/mailman/httpd/mailman.conf" ],
+        source => [ "puppet:///modules/site-mailman/httpd/${fqdn}/mailman.conf",
+                    "puppet:///modules/site-mailman/httpd/mailman.conf",
+                    "puppet:///modules/mailman/httpd/mailman.conf" ],
         require => Package[apache],
         notify => Service[apache],
         owner => root, group => 0, mode => 0644;
