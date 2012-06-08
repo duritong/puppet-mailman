4d56b308778f6d8017a96af5c4dc99efafd76331
mh <mh@immerda.ch>
2012-06-08 12:47:02 -0300

    refactor things for >2.7

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,24 +3,24 @@
 #
 # Copyright 2009, admin(at)immerda.ch
 #
-# This program is free software; you can redistribute 
-# it and/or modify it under the terms of the GNU 
-# General Public License version 3 as published by 
+# This program is free software; you can redistribute
+# it and/or modify it under the terms of the GNU
+# General Public License version 3 as published by
 # the Free Software Foundation.
 #
 
 class mailman(
-  $password = trocla("mailman.root.${fqdn}",'plain'),
+  $password = trocla("mailman.root.${::fqdn}",'plain'),
   $admin = hiera('mailman_admin'),
   $mailserver = hiera('mailman_mailserver'),
   $webserver = hiera('mailman_webserver')
 ) {
   include apache
-  case $operatingsystem {
+  case $::operatingsystem {
     centos: { include mailman::centos }
     base: { include mailman::base }
   }
-  if $use_munin {
+  if hiera('use_munin',false) {
     include mailman::munin
   }
 }
