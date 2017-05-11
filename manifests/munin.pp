ebe7cb6506e9ecc254e634aee4e2368b0c61a0a2
mh <mh@immerda.ch>
2017-05-11 11:48:01 +0200

    we need the package for the group munin

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,12 +1,14 @@
+# manage munin plugin
 class mailman::munin {
   file{'/var/lib/munin/plugin-state/munin-mailman-log.state':
-    ensure => file,
+    ensure  => file,
     replace => false,
-    owner => nobody, group => mailman, mode => 0640;
-  }
-  munin::plugin{'mailman':
+    require => Package['mailman'],
+    owner   => nobody,
+    group   => mailman,
+    mode    => '0640';
+  } -> munin::plugin{'mailman':
     config => "timeout 40
 group mailman",
-    require => File['/var/lib/munin/plugin-state/munin-mailman-log.state'];
   }
 }
