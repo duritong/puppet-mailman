2858eeea14260cb938565f90015bccd812c157e0
mh <mh@immerda.ch>
2022-08-05 11:25:06 +0200

    update state location

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,6 +1,6 @@
 # manage munin plugin
 class mailman::munin {
-  file{'/var/lib/munin/plugin-state/munin-mailman-log.state':
+  file{'/var/lib/munin-node/plugin-state/munin-mailman-log.state':
     ensure  => file,
     replace => false,
     require => Package['mailman'],
